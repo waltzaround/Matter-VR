db2ae567fda7576a657e919845b10da0f171dcee
waltzaround <vxs4050@autuni.ac.nz>
2016-04-16 16:49:10 +1200

    MVP1 built

---
+++ MVP Version 1/Assets/Scripts/HandButtonBehaviour.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandButtonBehaviour : MonoBehaviour {
+
+	//public Transform Pivot;
+	public GameObject Button;
+	public bool ButtonPressed;
+	public Transform Pressedpos;
+	public Transform Homepos;
+	public float pressDelay;
+	private float nextPress;
+	public float buttonPressInstant;
+	//private float startTime;
+	//private float journeyLength;
+	//public float speed;
+
+	//private bool ButtoninTransit;
+	//public int RotationSpeed;
+	//public int RotationDetent1;
+	//public int RotationDetent2;
+
+	void Start () {
+		//rigidbody = GetComponent<Rigidbody> ();
+		ButtonPressed = false;
+		nextPress = 0;
+		//startTime = Time.time;
+		//journeyLength = Vector3.Distance(Homepos.position, Pressedpos.position);
+	}
+	
+
+	/*
+		if (Button.GetComponent<Rigidbody>().rotation.x > RotationDetent1 && RotationSpeed > 0) 
+		{
+			Debug.Log ("STOP");
+			ButtoninTransit = false;
+		}
+		if (Button.GetComponent<Rigidbody>().rotation.x < RotationDetent2 && RotationSpeed < 0) 
+		{
+			Debug.Log ("STOP");
+			ButtoninTransit = false;
+		}*/
+	//}
+
+	void FixedUpdate () {
+		if (ButtonPressed == true) {
+			//float distCovered = (Time.time - startTime) * speed;
+			//float fracJourney = distCovered / journeyLength;
+			//transform.position = Vector3.Lerp(Homepos.position, Pressedpos.position, fracJourney);
+			GetComponent<Rigidbody> ().MovePosition (Pressedpos.position);
+			//ButtonPressed = true;
+			/*Debug.Log ("Pressed");
+			RotationSpeed = -RotationSpeed;
+			ButtoninTransit = true;*/
+		} else {
+			//float distCovered = (Time.time - startTime) * speed;
+			//float fracJourney = distCovered / journeyLength;
+			//transform.position = Vector3.Lerp(Pressedpos.position, Homepos.position, fracJourney);
+			GetComponent<Rigidbody> ().MovePosition (Homepos.position);
+			//ButtonPressed = false;
+		}
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.CompareTag ("Hand") && ButtonPressed == false && Time.time > nextPress) {
+			nextPress = Time.time + pressDelay;
+			ButtonPressed = true;
+		}
+		else if (other.gameObject.CompareTag ("Hand") && ButtonPressed == true && Time.time > nextPress) {
+			nextPress = Time.time + pressDelay;
+			ButtonPressed = false;
+		}
+	}
+
+	void OnMouseDown() {
+		if (Time.time > nextPress && ButtonPressed == false) {
+			nextPress = Time.time + pressDelay;
+			ButtonPressed = true;
+			buttonPressInstant = Time.time;
+			Debug.Log (buttonPressInstant + "Time: " + Time.time);
+		}
+		else if (Time.time > nextPress) {
+			nextPress = Time.time + pressDelay;
+			ButtonPressed = false;
+			buttonPressInstant = Time.time;
+			Debug.Log (buttonPressInstant + "Time: " + Time.time);
+		}
+	}
+
+	/*void OnTriggerExit (Collider other) {
+		if (other.gameObject.CompareTag ("Hand")) {
+			ButtonPressed = false;
+		}
+	}*/
+
+	/*void SmoothRotate () {
+		Button.transform.RotateAround (Pivot.position, Pivot.right, RotationSpeed * Time.deltaTime);
+	}*/
+}
---
+++ MVP Version 1/Assets/Scripts/TimerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimerController : MonoBehaviour {
+
+	public TextMesh textToDisplay;
+	private float millis;
+	private float sec;
+	private float min;
+	public HandButtonBehaviour Playtab;
+	public HandButtonBehaviour Stoptab;
+
+	// Use this for initialization
+	void Start () {
+		millis = 0;
+		sec = 0;
+		min = 0;
+		HandButtonBehaviour Playtab = GameObject.Find ("Playtab").GetComponent<HandButtonBehaviour> ();
+		HandButtonBehaviour Stoptab = GameObject.Find ("Stoptab").GetComponent<HandButtonBehaviour> ();
+		textToDisplay = gameObject.GetComponent<TextMesh> ();
+			//gameObject.GetComponent<TextMesh> ();
+			//gameObject.GetComponent("TextMesh") as TextMesh;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		textToDisplay.text = min.ToString ("F0") + ":" + sec.ToString ("F0") + ":" + millis.ToString ("F0");
+		if (Playtab.ButtonPressed == true) {
+			startTheClock ();
+		}
+		if (Stoptab.ButtonPressed == true) {
+			stopTheClock ();
+			Playtab.ButtonPressed = false;
+		}
+	}
+
+	public void startTheClock() {
+		millis += Time.deltaTime * 100;
+		if (millis >= 100) {
+			sec += 1;
+			millis = 0;
+		}
+		if (sec >= 60) {
+			min += 1;
+			sec = 0;
+		}
+	}
+
+	public void stopTheClock() {
+		millis = 0;
+		sec = 0;
+		min = 0;
+	}
+}
+//min.ToString () + ":" + sec.ToString () + ":" + millis.ToString ()

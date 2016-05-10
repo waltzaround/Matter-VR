4e56b06be23a1c3441f9ad87cb065dc0cd6e741f
Christopher Liu <christopherliu97@gmail.com>
2016-05-10 13:49:30 +1200

    Added UI Asset scene and drum instrument.

---
+++ MVP Version 1/Assets/DrumBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrumBehaviour : MonoBehaviour {
+
+	private AudioSource audio;
+
+	void Start() {
+		audio = GetComponent<AudioSource> ();
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (!collision.gameObject.CompareTag ("Constraint")) {
+			audio.Play ();
+		}
+	}
+}
